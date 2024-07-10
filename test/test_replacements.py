032c13f52258212a68e32d77e299c52108425bb1
Allison Beemer <66966224+anbeemer@users.noreply.github.com>
2024-07-10 10:45:35 -0500

    Update test_replacements to pytest

--- test/test_replacements.py
+++ test/test_replacements.py
@@ -1,48 +1,52 @@
-
 import sys
 sys.path.insert(0,'../')
 import markdown2canvas as mc
 import canvasapi
 
-import unittest
-
 import pytest
 
-class PageTester(unittest.TestCase):
-	@classmethod
-	def setUpClass(self):
-		import os
-		
-		from course_id import test_course_id
-		from canvas_url import canvas_url
-		self.canvas = mc.make_canvas_api_obj(url=canvas_url)
-		self.course = self.canvas.get_course(test_course_id) 
+@pytest.fixture(scope='class')
+def course():
+	import os
+
+	from course_id import test_course_id
+	from canvas_url import canvas_url
+	canvas = mc.make_canvas_api_obj(url=canvas_url)
+	
+	yield canvas.get_course(test_course_id) 
 
 
 
-		self.page_using_defaults = mc.Page('uses_replacements_default')
-		self.page_using_custom = mc.Page('uses_replacements_custom')
+@pytest.fixture(scope='class')
+def page_using_defaults(course):
+	import os
+	folder = 'uses_replacements_default'
+	filename = os.path.split(folder)[1]
+
+	##Not sure about filename - what was it being used for in "test_assignment"?
+	yield mc.Page(folder)
 
 
-		
+@pytest.fixture(scope='class')
+def page_using_custom(course):
+	import os
+	folder = 'uses_replacements_custom'
+	filename = os.path.split(folder)[1]
 
-	@classmethod
-	def tearDownClass(self):
-		self.canvas
-
-
-
-
-
-	def test_bbb_can_publish(self):
-		self.page_using_defaults.publish(self.course,overwrite=True)
-		self.page_using_custom.publish(self.course,overwrite=True)
+	yield mc.Page(folder)
 
 
-	def test_ddd_something_bad_happens(self):
+class TestPage():
+
+	def test_can_publish(self, course, page_using_defaults, page_using_custom):
+		page_using_defaults.publish(course,overwrite=True)
+		page_using_custom.publish(course,overwrite=True)
+
+
+	##Removed a " as e_info" after the def in the following... doesn't seem to have hurt it?
+	def test_missing_replacements(self):
 		# constructing a page with a replacements file that doesn't exist should raise
-
-		with pytest.raises(FileNotFoundError) as e_info:
+		with pytest.raises(FileNotFoundError):
 			mc.Page('uses_replacements_replacementsfile_doenst_exist')
 		
 
@@ -50,6 +54,3 @@ class PageTester(unittest.TestCase):
 
 
 
-if __name__ == '__main__':
-    pgnm = 'this_argument_is_ignored_but_necessary'
-    unittest.main(argv=[pgnm], exit=False)
