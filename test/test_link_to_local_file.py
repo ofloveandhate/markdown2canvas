2d6b03416ceba64b9f8740b92c02b4847b617060
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2024-07-10 11:59:54 -0500

    updated to use pytest

--- test/test_link_to_local_file.py
+++ test/test_link_to_local_file.py
@@ -1,75 +1,76 @@
 
 import sys
-sys.path.insert(0,'../')
-import markdown2canvas as mc
-import canvasapi
+sys.path.insert(0,'../') # this is the path to the thing that's being tested
+import markdown2canvas as mc # this is the thing that's being tested
 
-import unittest
+import canvasapi # dependency
+
+import pytest
 
 
-class PageTester(unittest.TestCase):
-	@classmethod
-	def setUpClass(self):
-		import os
-		
-		from course_id import test_course_id
-		from canvas_url import canvas_url
-		self.canvas = mc.make_canvas_api_obj(url=canvas_url)
-		self.course = self.canvas.get_course(test_course_id) 
+@pytest.fixture(scope='class')
+def course():
+	import os
 
-		self.folder = 'has_local_files'
-		self.filename = os.path.split(self.folder)[1]
-
-		self.page = mc.Page(self.folder)
-
-	@classmethod
-	def tearDownClass(self):
-		self.canvas
+	from course_id import test_course_id
+	from canvas_url import canvas_url
+	canvas = mc.make_canvas_api_obj(url=canvas_url)
+	
+	yield canvas.get_course(test_course_id) 
 
 
 
-	def test_aaa_meta(self):
-		self.assertEqual(self.page.name,'Test Has Local Files')
+@pytest.fixture(scope='class')
+def page(course):
+	import os
+	folder = 'has_local_files'
+	filename = os.path.split(folder)[1]
+
+	yield mc.Page(folder)
 
 
 
 
-	def test_bbb_can_publish(self):
-		self.page.publish(self.course,overwrite=True)
+class TestPageWithLocalFile():
+
+	def test_meta(self, course, page):
+		assert page.name == 'Test Has Local Files'
 
 
 
-	def test_already_online_raises(self):
+
+	def test_can_publish(self, course, page):
+		page.publish(course,overwrite=True)
+
+
+
+	def test_already_online_raises(self, course, page):
 		# publish once, forcefully.
-		self.page.publish(self.course,overwrite=True)
+		page.publish(course,overwrite=True)
 
 		# the second publish, with overwrite=False, should raise
-		with self.assertRaises(mc.AlreadyExists):
-			self.page.publish(self.course,overwrite=False) # default is False
+		with pytest.raises(mc.AlreadyExists):
+			page.publish(course,overwrite=False) # default is False
 
 
 
 
 
-	def test_yyy_doesnt_find_deleted(self):
-		name = self.page.name
+	def test_doesnt_find_deleted(self, course, page):
+		name = page.name
 
-		self.page.publish(self.course,overwrite=True)
-		self.assertTrue(mc.is_page_already_uploaded(name,self.course))
-		f = mc.find_page_in_course(name,self.course)
+		page.publish(course,overwrite=True)
+		assert mc.is_page_already_uploaded(name,course)
+		f = mc.find_page_in_course(name,course)
 		f.delete()
-		# print([i.name for i in self.course.get_pages()])
-		self.assertTrue(not mc.is_page_already_uploaded(name,self.course))
+		# print([i.name for i in course.get_pages()])
+		assert not mc.is_page_already_uploaded(name,course)
 
 
 
-	def test_zzz_can_find_published(self):
-		self.page.publish(self.course,overwrite=True)
-		self.assertTrue(mc.is_page_already_uploaded(self.page.name,self.course))
+	def test_can_find_published(self, course, page):
+		page.publish(course,overwrite=True)
+		assert mc.is_page_already_uploaded(page.name,course)
 
 
 
-
-if __name__ == '__main__':
-    pgnm = 'this_argument_is_ignored_but_necessary'
-    unittest.main(argv=[pgnm], exit=False)
