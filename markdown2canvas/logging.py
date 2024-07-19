525cb9c6a039f305230bc68c2ebda1c03ee61a1e
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2024-07-19 14:01:06 -0500

    added `default_` prefix
    to make more clear that setting them does nothing.  you have to call functions using them!

--- markdown2canvas/logging.py
+++ markdown2canvas/logging.py
@@ -3,7 +3,7 @@ Logging utilities.  Uses the built-in `logging` library.  This part could probab
 '''
 
 __all__ = [
-    'today', 'log_dir', 'logger', 'file_handler'
+    'today', 'default_log_dir', 'logger', 'file_handler'
     ]
 
 import os.path as path
@@ -16,31 +16,31 @@ import logging
 import datetime
 today = datetime.datetime.today().strftime("%Y-%m-%d")
 
-log_level=logging.DEBUG
+default_log_level=logging.DEBUG
 
-log_dir = path.join(path.normpath(os.getcwd()), '_logs')
+default_log_dir = path.join(path.normpath(os.getcwd()), '_logs')
 
-if not path.exists(log_dir):
-    os.mkdir(log_dir)
+if not path.exists(default_log_dir):
+    os.mkdir(default_log_dir)
 
-log_filename = path.join(log_dir, f'markdown2canvas_{today}.log')
+log_filename = path.join(default_log_dir, f'markdown2canvas_{today}.log')
 
 
-log_encoding = 'utf-8'
+default_log_encoding = 'utf-8'
 
 # make a logger object.  we'll getLogger in the other files as needed.
 
 logger = logging.getLogger() # make a root-level logger using the defaulted options.  see https://stackoverflow.com/questions/50714316/how-to-use-logging-getlogger-name-in-multiple-modules
 
 # adjust the logger for THIS module
-logger.setLevel(log_level)
+logger.setLevel(default_log_level)
 
 # make a file handler and attach
-file_handler = logging.FileHandler(log_filename, 'a', log_encoding)
+file_handler = logging.FileHandler(log_filename, 'a', default_log_encoding)
 logger.addHandler(file_handler)
 
 # a few messages to start
 logging.debug(f'starting logging at {datetime.datetime.now()}')
-logging.debug(f'reducing logging level of `requests` and `canvasapi` to WARNING')
+logging.debug(f'setting logging level of `requests` and `canvasapi` to WARNING')
 logging.getLogger('canvasapi.requester').setLevel(logging.WARNING)
 logging.getLogger('requests').setLevel(logging.WARNING)
